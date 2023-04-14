98553f1fc270a8f2c70de79d7fc5a0aa5250ae6f
rodriguesrai <rairodrigues_sf@hotmail.com>
2023-04-14 16:21:46 -0300

    requisito 3 completo

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 const cores = document.getElementsByClassName('color')
-cores = cores[0].style.backgroundColor = 'black';
+cores[0].style.backgroundColor = 'black';
+cores[1].style.backgroundColor = 'red';
+cores[2].style.backgroundColor = 'blue';
+cores[3].style.backgroundColor = 'pink';
 
-const mudaCores = () => {
-    for (index = 1; index < cores.length; index ++) {
-        cores[index].style.backgroundColor = 'black';
+
+/* const mudaCores = () => {
+    for (let index = 1; index < cores.length; index ++) {
+        const corAleatoria = Math.floor(Math.random() * cores.length);
+        cores[index].style.backgroundColor = cores[corAleatoria];
     }
 }
+
+/* const mudaCores = () => {
+    
+    for (let index = 1; index < cores.length; index ++) {
+        cores[index].style.backgroundColor = 'black';
+    }
+} */
