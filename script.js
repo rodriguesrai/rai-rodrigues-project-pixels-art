35fef5b8f3f853c71fd8a380d935e5b546a8c319
rodriguesrai <rairodrigues_sf@hotmail.com>
2023-04-17 16:00:21 -0300

    requisito 7 completo

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ pixelBoard.id = 'pixel-board';
 let newPixelBoard = document.body;
 newPixelBoard.appendChild(pixelBoard);
 
-
+//requisito 6
 const guardaQuadros = document.getElementById('pixel-board')
 for (let index = 0; index < 25; index += 1) {
     let divs = document.createElement('div');
