0410dd4bc4f7ff09ac1cfe5f7adde9eaf79e5d54
rodriguesrai <rairodrigues_sf@hotmail.com>
2023-04-17 12:44:20 -0300

    requisito 2-3 completos

--- script.js
+++ script.js
@@ -1,27 +1,47 @@
-const cores = document.getElementsByClassName('color');
+const cores = document.querySelectorAll('.color');
 cores[0].style.backgroundColor = 'rgb(0, 0, 0)';
+cores[1].style.backgroundColor = 'rgb(0, 0, 1)';
+cores[2].style.backgroundColor = 'rgb(0, 0, 2)';
+cores[3].style.backgroundColor = 'rgb(0, 0, 3)';
 
 let clickReset = document.getElementById('button-random-color');
-let coresGeradas = [];
-let geraCores = () => {
-    let red = Math.round(Math.random() * 255);
-    let green = Math.round(Math.random() * 255);
-    let blue = Math.round(Math.random() * 255);
 
-    redGreenBlue = `rgb(${red}, ${green}, ${blue})`;
+let coresGeradas = ['rgb(0, 0, 0)'];
+
+let geraCores = () => {
+    
+    let novaCor = newRGB();
+    
+    while (coresGeradas.includes(novaCor)) {
+        novaCor = newRGB();
+    }
+
+ 
+   coresGeradas.push(novaCor);
+    return novaCor;
+}
+
+function newRGB() {
+    let red = Math.floor(Math.random() * 255);
+    let green = Math.floor(Math.random() * 255);
+    let blue = Math.floor(Math.random() * 255);
+
+    let redGreenBlue = `rgb(${red}, ${green}, ${blue})`;
     return redGreenBlue;
 }
 
 const mudaCores = () => {
+
+    coresGeradas = ['rgb(0, 0, 0)']
     for (let index = 1; index < cores.length; index += 1) {
         cores[index].style.backgroundColor = geraCores();
     }
+   
 }
 
-
-
-/* coresGeradas.push(`rgb(${red}, ${green}, ${blue})`)
- 
-cores[index].style.backgroundColor = `rgb(${red}, ${green}, ${blue})`; */
-
 clickReset.addEventListener('click', mudaCores);
+
+
+
+/* localStorage.setItem('colorPalette', JSON.stringify(coresGeradas));
+let recoverySessionStorage = localStorage.getItem('colorPalette', JSON.parse(coresGeradas)); */
