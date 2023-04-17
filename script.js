4b356333715e56c36b0850822821acb3eebc56cf
rodriguesrai <rairodrigues_sf@hotmail.com>
2023-04-17 15:42:30 -0300

    requisito 6 completo

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 const cores = document.querySelectorAll('.color');
 cores[0].style.backgroundColor = 'rgb(0, 0, 0)';
-cores[1].style.backgroundColor = 'rgb(0, 0, 1)';
-cores[2].style.backgroundColor = 'rgb(0, 0, 2)';
-cores[3].style.backgroundColor = 'rgb(0, 0, 3)';
+cores[1].style.backgroundColor = 'rgb(0, 100, 1)';
+cores[2].style.backgroundColor = 'rgb(100, 0, 2)';
+cores[3].style.backgroundColor = 'rgb(0, 0, 100)';
+
+
+let pixelBoard = document.createElement('div');
+pixelBoard.id = 'pixel-board';
+let newPixelBoard = document.body;
+newPixelBoard.appendChild(pixelBoard);
+
+
+const guardaQuadros = document.getElementById('pixel-board')
+for (let index = 0; index < 25; index += 1) {
+    let divs = document.createElement('div');
+    divs.classList.add('pixel');
+    guardaQuadros.appendChild(divs);
+}
+const corInicial = document.querySelectorAll('.pixel');
+for (let index of corInicial) {
+    index.style.backgroundColor = 'rgb(255, 255, 255)'; 
+}
+/* corInicial.style.backgroundColor = 'rgb(255, 255, 255'; */
 
 let clickReset = document.getElementById('button-random-color');
 
